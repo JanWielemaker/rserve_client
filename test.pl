6e9324031210d8a0e4b7170e45943b81dac8645b
Jan Wielemaker <J.Wielemaker@cs.vu.nl>
2016-07-12 20:58:07 +0200

    Use r_assign()

--- test.pl
+++ test.pl
@@ -2,7 +2,7 @@
 
 cp(In, Out) :-
 	r_open(R, []),
-	r_set(R, v, In),
+	r_assign(R, v, In),
 	r_eval(R, v, Out),
 	r_close(R).
 
@@ -11,14 +11,14 @@ r :-
 	assertion(R==r).
 
 mean(List, Mean) :-
-	r_set(r, v, List),
+	r_assign(r, v, List),
 	r_eval(r, "mean(v)", Mean).
 
 iris(IRIS) :-
 	r_eval(r, "data(iris); iris", IRIS).
 
 svg(Data) :-
-	r_set(r, a, Data),
+	r_assign(r, a, Data),
 	r_eval(r, "svg(\"x.svg\")", _),
 	r_eval(r, "plot(a)", _),
 	r_eval(r, "dev.off()", _).
