44d93792b4d04a733f031ded4b8235e07fc61f77
Jan Wielemaker <J.Wielemaker@cs.vu.nl>
2016-08-09 15:55:44 +0200

    Bump version

--- pack.pl
+++ pack.pl
@@ -1,5 +1,5 @@
 name('rserve_client').
-version('0.4').
+version('0.5').
 title('R Rserve client').
 keywords(['R', 'statistics', 'chart', 'plot']).
 author( 'Jan Wielemaker', 'J.Wielemaker@cwi.nl' ).
