11c7b7059b27839bba2e486386a3a1f7e91d9e4f
Jan Wielemaker <J.Wielemaker@cs.vu.nl>
2016-08-10 16:14:47 +0200

    Update pack metadata

--- pack.pl
+++ pack.pl
@@ -1,9 +1,9 @@
 name('rserve_client').
-version('0.5').
+version('0.6').
 title('R Rserve client').
 keywords(['R', 'statistics', 'chart', 'plot']).
 author( 'Jan Wielemaker', 'J.Wielemaker@cwi.nl' ).
 packager( 'Jan Wielemaker', 'J.Wielemaker@cwi.nl' ).
 maintainer( 'Jan Wielemaker', 'J.Wielemaker@cwi.nl' ).
-home( 'http://www.swi-prolog.org' ).
-download( 'http://www.swi-prolog.org/download/swipl-rserve-client-*.tgz' ).
+home( 'https://github.com/JanWielemaker/rserve_client' ).
+download( 'https://github.com/JanWielemaker/rserve_client/releases/V*.zip' ).
