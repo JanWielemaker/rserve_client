48a46160bc2768182be757ab179c26935db41de7
Jan Wielemaker <J.Wielemaker@vu.nl>
2021-06-22 16:19:46 +0200

    Updated pack info and avoid eol//0 warning.

--- pack.pl
+++ pack.pl
@@ -1,9 +1,9 @@
 name('rserve_client').
-version('1.1.0').
+version('1.1.1').
 title('R Rserve client').
 keywords(['R', 'statistics', 'chart', 'plot']).
-author( 'Jan Wielemaker', 'J.Wielemaker@cwi.nl' ).
-packager( 'Jan Wielemaker', 'J.Wielemaker@cwi.nl' ).
-maintainer( 'Jan Wielemaker', 'J.Wielemaker@cwi.nl' ).
+author( 'Jan Wielemaker', 'jan@swi-prolog.org' ).
+packager( 'Jan Wielemaker', 'jan@swi-prolog.org' ).
+maintainer( 'Jan Wielemaker', 'jan@swi-prolog.org' ).
 home( 'https://github.com/JanWielemaker/rserve_client' ).
 download( 'https://github.com/JanWielemaker/rserve_client/releases/V*.zip' ).
